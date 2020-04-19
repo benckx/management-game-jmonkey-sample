f414c80d42066ded422e40f02af59d6684e0913e
benckx <benoit.vleminckx@restore.eu>
2020-04-19 12:14:51 +0200

    implement mouse wheel zoom

--- src/main/kotlin/be/encelade/bricks/GameApp.kt
+++ src/main/kotlin/be/encelade/bricks/GameApp.kt
@@ -10,6 +10,7 @@ import com.jme3.math.FastMath
 import com.jme3.scene.CameraNode
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
+import com.jme3.scene.shape.Sphere
 
 class GameApp : SimpleApplication() {
 
@@ -31,29 +32,42 @@ class GameApp : SimpleApplication() {
 
         inputManager.addListener(MyAnalogListener(this), "WHEEL_UP", "WHEEL_DOWN")
 
-        makeBoxFloor()
+        showOrigin()
+        addFloor()
     }
 
-    fun zoom(value: Float) {
-        val delta: Float = value * speed * 5
-        cameraNode.move(0f, -delta, 0f)
-        println("location: ${cameraNode.camera.location}")
+    private fun cameraZoom(value: Float) {
+        val delta = value * speed
+        val currentZ = cameraNode.camera.location.z
+        val newZ = currentZ + delta
+
+        if (newZ > MIN_Z && newZ < MAX_Z) {
+            cameraNode.move(0f, 0f, delta)
+        }
     }
 
     private class MyAnalogListener(val parent: GameApp) : AnalogListener {
         override fun onAnalog(name: String?, value: Float, tpf: Float) {
             when (name) {
-                "WHEEL_UP" -> parent.zoom(value)
-                "WHEEL_DOWN" -> parent.zoom(value)
+                "WHEEL_UP" -> parent.cameraZoom(-value)
+                "WHEEL_DOWN" -> parent.cameraZoom(value)
                 else -> println("Unknown $name")
             }
         }
     }
 
-    private fun makeBoxFloor() {
+    private fun showOrigin() {
+        val origin = Geometry("origin", Sphere(10, 10, 0.1f))
+        val material = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
+        material.setColor("Color", ColorRGBA.Red)
+        origin.material = material
+        rootNode.attachChild(origin)
+    }
+
+    private fun addFloor() {
         // replace Box by Surface
-        val floor = Geometry("floor", Box(5f, 0.01f, 5f))
-        floor.move(0f, 0f, 0f)
+        val size = 5f
+        val floor = Geometry("floor", Box(size, size, 0.01f))
         val mat = Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
         mat.setColor("Color", ColorRGBA.Gray)
         floor.material = mat
@@ -61,12 +75,14 @@ class GameApp : SimpleApplication() {
     }
 
     private fun enableTopViewMode() {
-        cameraNode.move(0f, 15f, 0f)
-        cameraNode.rotate(FastMath.HALF_PI, 0f, 0f)
+        cameraNode.move(0f, 0f, 5f)
+        cameraNode.rotate(FastMath.PI, 0f, 0f)
     }
 
     companion object {
-        const val MAX_Z = 100
-        const val MIN_Z = 5
+
+        const val MIN_Z = 2
+        const val MAX_Z = 40
+
     }
 }
